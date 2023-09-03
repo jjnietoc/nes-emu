875bef99169432ed6393521a5a65619bfaae5240
jjnietoc <juanjonietocastillo@gmail.com>
2023-09-03 19:30:16 +1000

    change: move struct functions to public so they can access and modify bit flags

--- cpu.hpp
+++ cpu.hpp
@@ -8,26 +8,29 @@
 class CPU
 {
 private:
-    // parts
+    /* --- registers --- */
     uint16_t pc;     // program counter
     uint8_t sp;      // stack pointer
     uint8_t A;       // accumulator
     uint8_t X;       // index register X
     uint8_t Y;       // index register Y
-    
-    enum class ProcessorFlags // flags of processor status
+
+    /* --- flags --- */
+    enum class ProcessorFlags // flags of processor status, bit 5 is unused
     {
       C = 1 << 0,   // Carry Flag
       Z = 1 << 1,   // Zero Flag
       I = 1 << 2,   // Interrupt Disable
       D = 1 << 3,   // Decimal Mode
       B = 1 << 4,   // Break Command
-      unused = 1 << 5,    // unused
       V = 1 << 6,   // Overflow Flag
       N = 1 << 7    // Negative Flag
     };
 
-    struct ProcessorStatus    // struct for setting, unsetting and checking flags in P 
+
+  public:
+    /* flag functions for setting, unsetting and checking P flags */
+    struct
     {
       uint8_t FlagValue = 0;
 
@@ -45,7 +48,7 @@ private:
       {
         return (FlagValue & (int)flag) == (int)flag;
       }
-    };
+    } ProcessorStatus;
 
     
 };
