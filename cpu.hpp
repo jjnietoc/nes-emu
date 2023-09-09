e8f90bed48ea164e365a14ecd4b22668273ad539
jjnietoc <juanjonietocastillo@gmail.com>
2023-09-09 12:54:59 +1000

    add: cpu base

--- cpu.hpp
+++ cpu.hpp
@@ -3,31 +3,69 @@
 
 #include <iostream>
 #include <stdint.h>
+#include "bus.hpp"
+
+enum AdressingModes {
+      Implicit,
+      Accumulator,
+      Immediate,
+      ZeroPage,
+      ZeroPageX,
+      ZeroPageY,
+      Absolute,
+      AbsoluteX,
+      AbsoluteY,
+      Relative,
+      Indirect,
+      IndirectX,
+      IndirectY
+    };
+
+class Bus;
 
 class CPU
 {
 private:
+    Bus *bus;
+
+public:
     /* --- registers --- */
     uint16_t pc;     // program counter
     uint8_t sp;      // stack pointer
     uint8_t A;       // accumulator
     uint8_t X;       // index register X
     uint8_t Y;       // index register Y
+    uint8_t P;    // Status Register
 
     /* --- flags --- */
     enum class ProcessorFlags // flags of processor status, bit 5 is unused
     {
-      Carry = 1 << 0,   // Carry Flag
-      Zero = 1 << 1,   // Zero Flag
-      InterruptDisable = 1 << 2,   // Interrupt Disable
-      Ddecimal = 1 << 3,   // Decimal Mode
-      Break = 1 << 4,   // Break Command
-      Overflow = 1 << 6,   // Overflow Flag
-      Negative = 1 << 7    // Negative Flag
+      C = 1 << 0,   // Carry Flag
+      Z = 1 << 1,   // Zero Flag
+      I = 1 << 2,   // Interrupt Disable
+      D = 1 << 3,   // Decimal Mode
+      B = 1 << 4,   // Break Command
+      U = 1 << 5,   // Unused
+      V = 1 << 6,   // Overflow Flag
+      N = 1 << 7    // Negative Flag
     };
 
+    /* --- addressing modes --- */
+    void AM_IMP();
+    void AM_ACC();
+    void AM_IMM();
+    void AM_ZP();
+    void AM_ZPX();
+    void AM_ZPY();
+    void AM_ABS();
+    void AM_ABSX();
+    void AM_ABSY();
+    void AM_REL();
+    void AM_IND();
+    void AM_INDX();
+    void AM_INDY();
 
-  public:
+
     /* flag functions for setting, unsetting and checking P flags */
     struct
     {
