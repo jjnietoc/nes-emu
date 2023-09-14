cc9e3a6e6777f32d5323ae559c7aba37b7cd2ce0
jjnietoc <juanjonietocastillo@gmail.com>
2023-09-14 16:33:05 +1000

    add: status flags functions

--- cpu.hpp
+++ cpu.hpp
@@ -8,36 +8,52 @@
 class chip2A03 
 {
   private:
-  /* registers */
-  uint8_t a;    // accumulator
-  uint8_t x;    // index x
-  uint8_t y;    // index y
+    /* registers */
+    uint8_t a;    // accumulator
+    uint8_t x;    // index x
+    uint8_t y;    // index y
 
-  uint16_t pc;  // program counter
-  uint8_t sp;   // stack pointer
+    uint16_t pc;  // program counter
+    uint8_t sp;   // stack pointer
+   
+    uint8_t flagRegisterStatus;
     
-  enum class statusFlags {
-    c = 1 << 0,   // carry
-    z = 1 << 1,   // zero
-    i = 1 << 2,   // interrupt disable
-    d = 1 << 3,   // decimal
-    b = 1 << 4,   // break
-    u = 1 << 5,   // unused
-    v = 1 << 6,   // overflow
-    n = 1 << 7    // negative
-  };
+    enum class statusFlag {
+      CARRY = 1 << 0,   // carry
+      ZERO = 1 << 1,   // zerO
+      INTERRUPT = 1 << 2,   // interrupt disable
+      DECIMAL = 1 << 3,   // decimal
+      BREAK = 1 << 4,   // break
+      U = 1 << 5,   // unused
+      OVERFLOW = 1 << 6,   // overflow
+      NEGATIVE = 1 << 7    // negative
+    };
 
-  // funcs to set flags
+    enum flagSet {
+      SET = 0,
+      UNSET = 1
+    };
+
+
+    // funcs to set flags
     // basically set every flag for future execution
+    void setStatusFlag(statusFlag sf, flagSet fs);
+    void setCarry(flagSet fs);
+    void setZero(flagSet fs);
+    void setInterrupt(flagSet fs);
+    void setDecimal(flagSet fs);
+    void setBreak(flagSet fs);
+    void setOverflow(flagSet fs);
+    void setNegative(flagSet fs);
 
-  enum memoryAccessMode {
+    enum memoryAccessMode {
       READ,
       WRITE
     };
 
-  /* addressing modes */
+    /* addressing modes */
 
-  /* instructions */
+    /* instructions */
 
 
 
