669bc3d5632763b5a3bc1b3777376032cc324260
jjnietoc <juanjonietocastillo@gmail.com>
2023-09-17 17:17:26 +1000

    add: func declarations for instructions

--- cpu.hpp
+++ cpu.hpp
@@ -69,6 +69,62 @@ class chip2A03
     /* Option 1: make funcs of all instructions, and just match them
      * Option 2: store them in array like chip8, check architecture
      * Option 3: make struct and feed a decoder what it needs */
+    void ADC();
+    void AND();
+    void ASL();
+    void BCC();
+    void BCS();
+    void BEQ();
+    void BIT();
+    void BMI();
+    void BNE();
+    void BPL();
+    void BRK();
+    void BVC();
+    void BVS();
+    void CLC();
+    void CLD();
+    void CLI();
+    void CLV();
+    void CMP();
+    void CPX();
+    void DEC();
+    void DEX();
+    void DEY();
+    void EOR();
+    void INC();
+    void INX();
+    void INY();
+    void JMP();
+    void JSR();
+    void LDA();
+    void LDX();
+    void LDY();
+    void LSR();
+    void NOP();
+    void ORA();
+    void PHA();
+    void PHP();
+    void PLA();
+    void PLP();
+    void ROL();
+    void ROR();
+    void RTI();
+    void RTS();
+    void SBC();
+    void SEC();
+    void SED();
+    void SEI();
+    void STA();
+    void STX();
+    void STY();
+    void TAX();
+    void TAY();
+    void TSX();
+    void TXA();
+    void TXS();
+    void TYA();
+
 
     //* ---- WIP ---- *//
     /* instructions */
