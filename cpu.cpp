725fab8f04af208d2ea58639c0db137a6c8f9215
jjnietoc <juanjonietocastillo@gmail.com>
2023-09-19 18:29:39 +1000

    add: bitset check for register bit locations in instructions

--- cpu.cpp
+++ cpu.cpp
@@ -1,5 +1,7 @@
 #include "cpu.hpp"
 
+
+
 chip2A03::chip2A03() {
   // ram from ram.hpp?
   // i/o
@@ -57,3 +59,131 @@ void chip2A03::setNegative(flagSet fs) {
 //    };
 //   };
 // };
+
+//* instructions *//
+
+// Clear Decimal Mode
+void chip2A03::CLD() {
+  setDecimal(UNSET);
+}
+
+// Clear Interrupt Disable
+void chip2A03::CLI() {
+  setInterrupt(UNSET);
+};
+
+// Clear Overflow flag
+void chip2A03::CLV() {
+  setOverflow(UNSET);
+};
+
+// Decrement X Register
+void chip2A03::DEX() {
+  x--;
+  if(x == 0)
+    setZero(SET);
+
+  if(isXset)
+    setNegative(SET);
+};
+
+// Decrement Y Register
+void chip2A03::DEY() {
+  y--;
+  if(y == 0)
+    setZero(SET);
+
+  if(isYSet)
+    setNegative(SET);
+};
+
+// Increment X Register
+void chip2A03::INX() {
+  x++;
+  if(x == 0)
+    setZero(SET);
+
+  if(isXset)
+    setNegative(SET);
+};
+
+// Increment Y Register
+void chip2A03::INY() {
+  y++;
+  if(y == 0)
+    setZero(SET);
+
+  if(isYSet)
+    setNegative(SET);
+};
+
+// Set Carry Flag
+void chip2A03::SEC() {
+  setCarry(SET);
+};
+
+// Set Decimal Flag
+void chip2A03::SED() {
+  setDecimal(SET);
+};
+
+// Set Interrupt Disable
+void chip2A03::SEI() {
+  setInterrupt(SET);
+};
+
+// Transfer Accumulator to X
+void chip2A03::TAX() {
+  x = a;
+  if(x == 0)
+    setNegative(SET);
+
+  if(isXset)
+    setNegative(SET);
+};
+
+// Transfer Accumulator to Y
+void chip2A03::TAY() {
+  y = a;
+  if(y == 0)
+    setZero(SET);
+
+  if(isYSet)
+    setNegative(SET);
+}
+
+// Transfer Stack Pointer to X
+void chip2A03::TSX() {
+  sp = x;
+  if(x == 0)
+    setZero(SET);
+
+  if(isXset)
+    setNegative(SET);
+
+}
+
+// Transfer X to Accumulator
+void chip2A03::TXA() {
+  a = x;
+  if(a == 0)
+    setZero(SET);
+  if(isASet)
+    setNegative(SET);
+};
+
+// transfer X to Stack Pointer
+void chip2A03::TXS() {
+  x = sp;
+};
+
+// transfer Y to Accumulator
+void chip2A03::TYA() {
+  a = y;
+  if(a == 0)
+    setZero(SET);
+  if(isASet)
+    setNegative(SET);
+};
+
+
