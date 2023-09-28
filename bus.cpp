9f00c85209e3fe98192871aeca03f7287e04852a
jjnietoc <juanjonietocastillo@gmail.com>
2023-09-28 16:29:33 +1000

    add: todo lists for cpu and bus

--- bus.cpp
+++ bus.cpp
@@ -1,5 +1,10 @@
 #include "bus.hpp"
 
+// WIP
+// TODO
+// 1. Finish memory map when doing rest of the parts
+// 2. Write address read/write for PPU, APU and cartdriges
+
 uint8_t Bus::readCPU(uint16_t address) {
   return wRAM[address];
 }
@@ -8,12 +13,7 @@ void Bus::writeCPU(uint16_t address, uint8_t data) {
   data = wRAM[address];
 }
 
-// WIP
-// TODO:
-// - change this function to write only, this way we eliminate
-//   the need to compare enums
-// - create function for reading only
-// needs to implement rest of the parts
+
 void Bus::writeMemory(uint16_t address, uint8_t data) {
   if(address >= 0x0 & address < 0x07FF) {
       writeCPU(address, data);
