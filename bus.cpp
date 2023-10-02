4449caf631519742879ba2397e0a2be73b2322cf
jjnietoc <juanjonietocastillo@gmail.com>
2023-10-02 14:07:12 +1000

    add: fillMemory function to fill vector of Memory objects. Gives error as class is virtual

--- bus.cpp
+++ bus.cpp
@@ -5,18 +5,23 @@
 // 1. Finish memory map when doing rest of the parts
 // 2. Write address read/write for PPU, APU and cartdriges
 
-uint8_t Bus::readCPU(uint16_t address) {
+uint8_t Bus::read(uint16_t address) {
   return wRAM[address];
 }
 
-void Bus::writeCPU(uint16_t address, uint8_t data) {
+void Bus::write(uint16_t address, uint8_t data) {
   data = wRAM[address];
 }
+
+void Bus::fillMemory() {
+  for(int i = 0; i != memory_size; ++i)
+    handler.std::push_back(std::make_unique<Memory>());
+};
 
 
 void Bus::writeMemory(uint16_t address, uint8_t data) {
   if(address >= 0x0 & address < 0x07FF) {
-      writeCPU(address, data);
+      write(address, data);
     };
 
   // PPU
@@ -38,7 +43,7 @@ void Bus::writeMemory(uint16_t address, uint8_t data) {
 
 void Bus::readMemory(uint16_t address) {
   if(address >= 0x0 & address < 0x07FF) {
-    readCPU(address);
+    read(address);
   }
   
   // PPU
@@ -58,3 +63,5 @@ void Bus::readMemory(uint16_t address) {
     return;
 
 }
+
+
