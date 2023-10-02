acfadc35d1ac53be18916eeeaaf2b25dd5bc223c
jjnietoc <juanjonietocastillo@gmail.com>
2023-10-02 14:05:38 +1000

    add: make class pure virtual

--- memory.hpp
+++ memory.hpp
@@ -9,10 +9,10 @@ class Memory
 {
   public:
     Memory();
-    ~Memory();
+    virtual ~Memory();
 
-    virtual uint8_t read(uint16_t address);
-    virtual void write(uint16_t address, uint8_t data);
+    virtual uint8_t read(uint16_t address) = 0;   // "= 0" makes the class abstract and the method pure virtual
+    virtual void write(uint16_t address, uint8_t data) = 0;
 };
 
 #endif
