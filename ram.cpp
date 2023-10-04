b81d1390dd52be67af28979754f81440de37c9da
jjnietoc <juanjonietocastillo@gmail.com>
2023-10-04 19:06:53 +1000

    remove: unused functions

--- ram.cpp
+++ ram.cpp
@@ -1,10 +1,5 @@
 #include "ram.hpp"
 
-// WIP
-// TODO
-// 1. Finish memory map when doing rest of the parts
-// 2. Write address read/write for PPU, APU and cartdriges
-
 uint8_t Ram::read(uint16_t address) {
   return wram[address];
 }
